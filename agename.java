25dea094dddc05ead687c2b6bf6a77da3b988af2
Sparash sharma <sparash.27kpr@gmail.com>
2018-10-24 09:24:50 +0000

    assignment15

--- agename.java
+++ agename.java
@@ -1,54 +1,67 @@
 import java.util.*;
+import java.io.*;
 
-class Student2018 implements Comparable <Student2018>
+class Student
 {
-	private int age;
-	private String name;
-	Student2018(int age,String name)
-	{
-		this.age=age;
-		this.name=name;
-	}
-	public int getAge()
-	{
-		return this.age;
-	}
-	public String getname() {
-		return this.name;
-	}
-	public int compareTo(Student2018 a2) {
-		if(this.getAge()>a2.getAge())
-			return 1;
-		else if(this.getAge()==a2.getAge())
-		{
-			if(this.getname().compareTo(a2.getname())>0)
-			{
-				return 1;
-			}
-			else if(this.getname().equals(a2.getname()))
-				return 0;
-			else
-				return -1;
-		}
-		else
-			return -1;
-	}
-
+	int age;
+	String name;
+	public Student(int age, String name)
+   {
+   this.age=age;
+   this.name=name;
+   }
+   public String toString()
+   {
+   return this.age + " " + this.name;
+   }
 }
-class Genric4
+class Sortbyage implements Comparator<Student>
 {
-	public static void main(String x[])
+public int compare(Student a,Student b)
+{
+Student s1=(Student)a;
+Student s2=(Student)b;
+
+if(s1.age==s2.age)
+return 0;
+else if(s1.age>s2.age)
+return 1;
+else
+return -1;
+ }
+}
+class Sortbyname implements Comparator<Student>
+{
+	public int compare(Student a, Student b)
 	{
-		Scanner scan = new Scanner(System.in);
-		LinkedList<Student2018> l1= new LinkedList<Student2018>();
-		for(int i=0;i<5;i++)
-		{
-			int age=scan.nextInt();
-			String name=scan.next();
-			l1.add(new Student2018(age,name));
-		}
-		l1.sort(null);
-		for(Student2018 i:l1)
-			System.out.println(i.getAge()+" "+i.getname());
+      return a.name.compareTo(b.name);
+	}
+}
+// Driver class
+class Demo
+{
+	public static void main(String[] args)
+	{
+      ArrayList<Student> ar =new ArrayList<Student>();
+      ar.add(new Student(18,"Sparash\t"));
+      ar.add(new Student(19,"Tushar\t"));
+      ar.add(new Student(20,"Shubham\t"));
+
+      System.out.println("Unsorted");
+      for(int i=0;i<ar.size();i++)
+      System.out.println(ar.get(i));
+
+      Collections.sort(ar,new Sortbyage());
+
+      System.out.println("\nSorted by age");
+      for(int i=0;i<ar.size();i++)
+      System.out.println(ar.get(i));
+
+      Collections.sort(ar,new Sortbyname());
+
+      System.out.println("\nSorted by name");
+      for(int i=0;i<ar.size();i++)
+      System.out.println(ar.get(i));
+
 	}
 }
--- gene.java
+++ gene.java
@@ -1,34 +1,25 @@
 import java.util.*;
-class Ext
+class que3
 {
-	public static <E> void sort123(E[] arrayb)
-	{
-		LinkedList<E> l1=new LinkedList<E>();
-		for(int i=0;i<arrayb.length;i++)
-		{
-			l1.add(arrayb[i]);
-		}
-		l1.sort(null);
-		for(E i:l1)
-		{
-			System.out.print(i+" ");
-		}
-		System.out.println();
+  public static <E> void printArray(E[] inputArray)
+  {
+    for (E element : inputArray)
+      System.out.printf("%s ", element);
 
-	}
+    System.out.println();
+  }
 
-}
-public class gene
-{
-	public static void main(String[] x)
-	{
-		Integer arr[]={2,3,1,6,4,7,5};
-		Double ar1[]={2.3,5.6,4.0,3.1,8.9};
-		String ar2[]={"abd","aab","bcd","cba"};
-		Character ar3[]={'a','b','c','d','e'};
-		Ext.sort123(arr);
-		Ext.sort123(ar1);
-		Ext.sort123(ar2);
-		Ext.sort123(ar3);
-	}
+  public static void main(String args[])
+  {
+    Integer[] integerArray = { 1, 2, 3, 4, 5, 6 };
+    Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7 };
+    Character[] characterArray = { 'H', 'E', 'L', 'L', 'O' };
+
+    System.out.println("Array integer Array contains:");
+    printArray(integerArray);
+    System.out.println("\nArray double Array contains:");
+    printArray(doubleArray);
+    System.out.println("\nArray character Array contains:");
+    printArray(characterArray);
+  }
 }
